58d0272de6cf38c467bd2a16ae6b8cd494a5ab3c
Stefan Eletzhofer <se@nexiles.de>
2016-07-08 11:41:30 +0200

    Hook in videos resource and refactor auth plug.

--- web/router.ex
+++ web/router.ex
@@ -21,10 +21,13 @@ defmodule Rumbl.Router do
     resources "/users",     UserController,       only: [:index, :show, :new, :create]
     resources "/sessions",  SessionController,    only: [:new, :create, :delete]
 
+
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Rumbl do
-  #   pipe_through :api
-  # end
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos",    VideoController
+  end
+
 end
